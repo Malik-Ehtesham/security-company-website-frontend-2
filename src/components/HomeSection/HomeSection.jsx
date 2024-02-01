8a050d9fef57c117dea2421bc32c306b8e56adbe
ehteshamzahid <ehteshamzahid313@gmail.com>
2024-02-01 15:34:12 +0500

    first commit

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -35,7 +35,7 @@ const HomeSection = () => {
             </Link>
             <Link
               to="/aboutus"
-              className="btn btn-outline   text-white  rounded-lg  md:btn-lg my-6  mx-4"
+              className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 border-2 my-2 border-gray-400 bg-black py-3 hover:border-gray-500 hover:bg-gray-500 px-8 font-semibold  text-white hover:text-black rounded-lg  mx-4 text-lg"
             >
               Erfahren Sie mehr
             </Link>
